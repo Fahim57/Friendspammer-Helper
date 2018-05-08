d1edfb3e8db28d584df261d3ac02f30ae96e0350
fahim57 <fahim_arab@hotmail.com>
2018-05-08 22:18:29 +0200

    Added sonar in wercker without space after token

--- src/test/java/com/example/project/Testing.java
+++ src/test/java/com/example/project/Testing.java
@@ -1,12 +1,13 @@
 package com.example.project;
 
 import static org.junit.Assert.fail;
+
 import org.junit.Test;
 
 public class Testing {
 
     @Test
     public void test_fail() {
-        fail("Let's test the failing unit test");
+        // fail("Let's test the failing unit test");
     }
 }
